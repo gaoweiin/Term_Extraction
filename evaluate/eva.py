21673c091fb1e3b074507c4bd987f3eebac11dca
Wei Gao <admin@gaowei.in>
2024-08-08 15:11:14 +0100

    Update eva.py

--- evaluate/eva.py
+++ evaluate/eva.py
@@ -48,6 +48,27 @@ def process_data(model_name,max_tokens,input_file,output_dir,prompt_format,syste
         except:
             print("Error in writing to file")
             print(da)
+def get_prompt_format(model_name):
+    if model_name == "phi3":
+        return r"""<s><|system|>{}<|end|>
+    <|user|>{}<|end|>
+    <|assistant|>{}"""
+    elif model_name == "llama3":
+        return r"""<|begin_of_text|><|start_header_id|>system<|end_header_id><|eot_id|>
+{}
+<|start_header_id|>user<|end_header_id|>{}<|eot_id|><|start_header_id|>assistant<|end_header_id|>{}"""
+    elif model_name == "gemma":
+        return r"""<start_of_turn>user
+    {}"{}" <end_of_turn>
+    <start_of_turn>model
+    {}"""
+    elif model_name == "qwen2":
+        return r"""<|im_start|>system
+    {}<|im_end|>
+    <|im_start|>user
+    {}<|im_end|>
+    <|im_start|>assistant<|eot_id|>
+    {}"""
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process medical text data.')
     parser.add_argument('--model', type=str, default="weiiv/llama3.1_term_json", help='Model')
@@ -58,5 +79,7 @@ if __name__ == "__main__":
 {}
 <|start_header_id|>user<|end_header_id|>{}<|eot_id|><|start_header_id|>assistant<|end_header_id|>{}""", help="prompt format")
     parser.add_argument("--system_prompt", type=str, default="Extract terms from the following data and format it in JSON using IOB notation.", help="system prompt")
+    parser.add_argument("--model_type", type=str, default="llama3", help="Model type")
     args = parser.parse_args()
-    process_data(args.model, args.max_tokens, args.input_file, args.output_dir, args.prompt_format, args.system_prompt)
+    prompt_format = get_prompt_format(args.model_type)
+    process_data(args.model, args.max_tokens, args.input_file, args.output_dir, prompt_format, args.system_prompt)
