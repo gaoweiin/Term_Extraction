cf838172b2da4ed18deef8cd2f1f5e3a4a3ddf8f
Wei Gao <admin@gaowei.in>
2024-08-08 15:22:17 +0100

    Update run_evaluate.sh

--- evaluate/run_evaluate.sh
+++ evaluate/run_evaluate.sh
@@ -14,10 +14,10 @@ Constraints: Focus mainly on extracting names and heart failure-related terms. I
 curl -o test.json https://huggingface.co/datasets/weiiv/terms_new/raw/main/test.json
 
 mkdir results_gemma
-python eva.py --model "gemma-2-9b-it-bnb-4bit" --max_tokens 4096 --input_file "test.json" --output_dir "results_gemma" --model_type "gemma" --system_prompt "Extract terms from the following data and format it in JSON using IOB notation."
+python eva.py --model "../train/gemma-2-9b-it-bnb-4bit" --max_tokens 4096 --input_file "test.json" --output_dir "results_gemma" --model_type "gemma" --system_prompt "Extract terms from the following data and format it in JSON using IOB notation."
 mkdir results_phi3
-python eva.py --model "Phi-3-medium-4k-instruct-bnb-4bit" --max_tokens 4096 --input_file "test.json" --output_dir "results_phi3" --model_type "phi3" --system_prompt "Extract terms from the following data and format it in JSON using IOB notation."
+python eva.py --model "../train/Phi-3-medium-4k-instruct-bnb-4bit" --max_tokens 4096 --input_file "test.json" --output_dir "results_phi3" --model_type "phi3" --system_prompt "Extract terms from the following data and format it in JSON using IOB notation."
 mkdir results_qwen2
-python eva.py --model "Qwen2-7B-Instruct-bnb-4bit" --max_tokens 4096 --input_file "test.json" --output_dir "results_qwen2" --model_type "qwen2" --system_prompt "Extract terms from the following data and format it in JSON using IOB notation."
+python eva.py --model "../train/Qwen2-7B-Instruct-bnb-4bit" --max_tokens 4096 --input_file "test.json" --output_dir "results_qwen2" --model_type "qwen2" --system_prompt "Extract terms from the following data and format it in JSON using IOB notation."
 mkdir results_llama3
-python eva.py --model "Meta-Llama-3.1-8B-prompt" --max_tokens 4096 --input_file "test.json" --output_dir "results_llama3" --model_type "llama3" --system_prompt $system_prompt_1
+python eva.py --model "../train/Meta-Llama-3.1-8B-prompt" --max_tokens 4096 --input_file "test.json" --output_dir "results_llama3" --model_type "llama3" --system_prompt $system_prompt_1
