409d8276eefb1eb07698e65bdb317e5f0ee8aeb4
Wei Gao <admin@gaowei.in>
2024-08-08 15:09:41 +0100

    Update run_evaluate.sh

--- evaluate/run_evaluate.sh
+++ evaluate/run_evaluate.sh
@@ -5,22 +5,8 @@ MODEL="weiiv/llama3.1_term_json"
 MAX_TOKENS=4096
 INPUT_FILE="test.json"
 OUTPUT_DIR="results"
-PROMPT_FORMAT="<|begin_of_text|><|start_header_id|>system<|end_header_id><|eot_id|>\n{}\n<|start_header_id|>user<|end_header_id|>{}<|eot_id|><|start_header_id|>assistant<|end_header_id|>{}"
 SYSTEM_PROMPT="Extract terms from the following data and format it in JSON using IOB notation."
-
-# Parse command line arguments
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        --model) MODEL="$2"; shift ;;
-        --max_tokens) MAX_TOKENS="$2"; shift ;;
-        --input_file) INPUT_FILE="$2"; shift ;;
-        --output_dir) OUTPUT_DIR="$2"; shift ;;
-        --prompt_format) PROMPT_FORMAT="$2"; shift ;;
-        --system_prompt) SYSTEM_PROMPT="$2"; shift ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-    shift
-done
+model_type="llama3"
 curl -o test.json https://huggingface.co/datasets/weiiv/terms_new/raw/main/test.json
 mkdir $OUTPUT_DIR
 # Run the Python script with the provided arguments
@@ -29,5 +15,5 @@ python3 eva.py \
     --max_tokens "$MAX_TOKENS" \
     --input_file "$INPUT_FILE" \
     --output_dir "$OUTPUT_DIR" \
-    --prompt_format "$PROMPT_FORMAT" \
+    --model_type "$model_type" \
     --system_prompt "$SYSTEM_PROMPT"
