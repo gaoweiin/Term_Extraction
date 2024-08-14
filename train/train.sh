b08454b8d3dd18c97c7687c163589bc57e5edcb4
Wei Gao <admin@gaowei.in>
2024-08-15 00:51:23 +0700

    fix missing "

--- train/train.sh
+++ train/train.sh
@@ -7,4 +7,4 @@ python train.py --model_name "unsloth/Phi-3-medium-4k-instruct-bnb-4bit" --num_e
 
 python train.py --model_name "unsloth/Qwen2-7B-Instruct-bnb-4bit" --num_epochs 1 --model_type "qwen2" --batch_size 8 --system_prompt "Extract terms from the following data and format it in JSON using IOB notation." --model_dir "Qwen2-7B-Instruct-bnb-4bit"
 
-python train.py --model_name "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit" --num_epochs 1 --model_type "llama3 --batch_size 8 --model_dir "Meta-Llama-3.1-8B-prompt"
+python train.py --model_name "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit" --num_epochs 1 --model_type "llama3" --batch_size 8 --model_dir "Meta-Llama-3.1-8B-prompt"
